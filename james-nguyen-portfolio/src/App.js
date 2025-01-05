e4cc70be92a20cb0dd56acb386429ad3329af14d
jamess-nguyen <haimenguyen@gmail.com>
2025-01-04 16:43:50 -0800

    :sparkles: built out carousel component

--- james-nguyen-portfolio/src/App.js
+++ james-nguyen-portfolio/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/header';
-import Block from './components/block/block';
 import Banner from './components/banner/banner';
+import Carousel from './components/carousel/carousel';
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
-      <Block />
+      <Carousel/>
     </>
   );
 }
---
+++ james-nguyen-portfolio/src/components/carousel/carousel.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import './carousel.css';
+import Block from '../block/block.js';
+
+const Carousel = () => {
+  const [selected, setSelected] = useState('projects'); 
+  const handleClick = (section) => {
+    setSelected(section);
+  };
+
+  const renderContent = () => {
+    switch (selected) {
+      case 'work':
+        return <p>Work Experience Content</p>; 
+      case 'projects':
+        return <Block/>
+      case 'awards':
+        return <p>Awards Content</p>;  
+      default:
+        return <p>Work Experience Content</p>;
+    }
+  };
+
+  return (
+    <div className="carousel-container">
+      <div className="button-container">
+        <button
+          className={`button ${selected === 'work' ? 'active' : ''}`}
+          onClick={() => handleClick('work')}
+        >
+          Work Experience
+        </button>
+        <button
+          className={`button ${selected === 'projects' ? 'active' : ''}`}
+          onClick={() => handleClick('projects')}
+        >
+          Projects
+        </button>
+        <button
+          className={`button ${selected === 'awards' ? 'active' : ''}`}
+          onClick={() => handleClick('awards')}
+        >
+          Awards
+        </button>
+      </div>
+      <div className="content-container">
+        {renderContent()}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
