ea949b080ffe6d31faf42640a5f0838cc9140d6d
jamess-nguyen <haimenguyen@gmail.com>
2025-01-18 15:37:45 -0800

    :sparkles: added work exp

--- src/components/workexp/workexp.js
+++ src/components/workexp/workexp.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './workexp.css';
+import logo192 from '../../assets/logo192.png'
 
 const Workexp = () => {
   return (
     <div className="work-experience-container">
         <div className="intro-container">
-            <p className="work-intro">Where I've Been</p>
+            <p className="work-intro">Where I've Worked:</p>
         </div>
       <div className="logos-row">
         <div className="logo-container">
-          <img src="path-to-usc-logo.png" alt="USC" className="logo" />
+          <img src={logo192} alt="USC" className="logo" />
         </div>
         <div className="logo-container">
-          <img src="path-to-mlb-logo.png" alt="MLB" className="logo" />
+          <img src={logo192} alt="MLB" className="logo" />
         </div>
         <div className="logo-container">
-          <img src="path-to-uci-logo.png" alt="UCI" className="logo" />
+          <img src={logo192} alt="UCI" className="logo" />
         </div>
         <div className="logo-container">
-          <img src="path-to-appjam-logo.png" alt="AppJam+" className="logo" />
+          <img src={logo192} alt="AppJam+" className="logo" />
         </div>
       </div>
     </div>
